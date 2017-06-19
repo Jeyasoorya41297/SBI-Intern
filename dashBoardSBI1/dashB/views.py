e37de846bc6d6049fdc6e1d597acaac4672dd61e
Jeyasoorya <jeys041297sts@gmail.com>
2017-06-19 17:24:26 +0530

    Home page

--- dashBoardSBI1/dashB/views.py
+++ dashBoardSBI1/dashB/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello World")
+	i_apps = Inward.objects.all()
+	o_no_apps = len(list(Outward.objects.all()))
+	o_comm_list = list(OutwardData.objects.all().order_by('-Date').values_list('commissionAmt'))[0:o_no_apps-1]
+	o_amt_list = list(OutwardData.objects.all().order_by('-Date').values_list('BeneficiaryAmountINR'))[0:o_no_apps-1]
+	o_comm = 0
+	for out in o_comm_list:
+		o_comm = o_comm+out[0]
+	o_amt = 0
+	for out in o_amt_list:
+		o_amt = o_amt+out[0]
+	i_no_apps = len(list(i_apps))
+	i_comm_list = list(InwardData.objects.all().order_by('-Date').values_list('GLS_commission'))[0:i_no_apps-1]
+	i_commex_list = list(InwardData.objects.all().order_by('-Date').values_list('Ex_commission'))[0:i_no_apps-1]
+	i_amt_list = list(InwardData.objects.all().order_by('-Date').values_list('amountINR'))[0:i_no_apps-1]
+	i_comm_gls = 0
+	for i in i_comm_list:
+		i_comm_gls = i_comm_gls+i[0]
+	i_comm_ex = 0
+	for i in i_commex_list:
+		i_comm_ex = i_comm_ex+i[0]
+	i_comm = i_comm_ex+i_comm_gls
+	i_amt = 0
+	for i in i_amt_list:
+		i_amt = i_amt+i[0]
+	return render(request,"dashB/index.html",{"inward":i_apps,"o_comm":o_comm,"o_amt":o_amt,"i_comm":i_comm,"i_amt":i_amt})
--- dashBoardSBI1/dashBoardSBI1/urls.py
+++ dashBoardSBI1/dashBoardSBI1/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^dashB/', include('dashB.urls')),
+	url(r'^dashb/', include('dashB.urls')),
     url(r'^admin/', admin.site.urls),
 ]
