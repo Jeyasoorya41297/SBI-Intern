ccbee1d7fcfc495049d6059e6625574dddd5d71c
Aadhavan Nambhi M <aadhavannambhi.m@gmail.com>
2017-06-19 15:43:19 +0530

    Populated database and added gitignore

--- dashBoardSBI1/dashB/models.py
+++ dashBoardSBI1/dashB/models.py
@@ -15,7 +15,7 @@ class InwardData(models.Model):
 	Date = models.DateField() 
 	App = models.ForeignKey(Inward, on_delete=models.CASCADE)
 	def __str__(self):
-		return self.App_name
+		return self.App.App_name+" "+str(self.Date)
 
 class Outward(models.Model):
 	Currency = models.CharField(max_length=10)
@@ -23,11 +23,11 @@ class Outward(models.Model):
 		return self.Currency
 
 class OutwardData(models.Model):
-	amountForeign = models.DecimalField(max_digits=15, decimal_places=2)
-	amountINR = models.DecimalField(max_digits=15, decimal_places=2)
-	GLS_commission = models.DecimalField(max_digits=15, decimal_places=2)
+	BeneficiaryAmount = models.DecimalField(max_digits=15, decimal_places=2)
+	BeneficiaryAmountINR = models.DecimalField(max_digits=15, decimal_places=2)
+	commissionAmt = models.DecimalField(max_digits=15, decimal_places=2)
 	Service_tax = models.DecimalField(max_digits=15, decimal_places=2)
 	Date = models.DateField()
 	curr = models.ForeignKey(Outward, on_delete=models.CASCADE) 
 	def __str__(self):
-		return self.App_name
+		return self.curr.Currency+" "+str(self.Date)
