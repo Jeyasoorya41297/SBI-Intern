e13fef5246ccd87f5736a732b331193c173a8748
Jeyasoorya <jeys041297sts@gmail.com>
2017-06-19 14:27:19 +0530

    created models

--- dashBoardSBI1/dashB/admin.py
+++ dashBoardSBI1/dashB/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Inward)
+admin.site.register(InwardData)
+admin.site.register(Outward)
+admin.site.register(OutwardData)
