3fa4d9acde83c3789a495e82437892bc2bc0ce9c
Raunaque <rpatra1997@gmail.com>
2017-06-20 13:10:03 +0530

    new models

--- dashBoardSBI/dashb/models.py
+++ dashBoardSBI/dashb/models.py
@@ -3,12 +3,24 @@ from django.db import models
 # Create your models here.
 
 
-class Students(models.Model):
-	name = models.CharField(max_length=20)
-	maths = models.IntegerField()
-	chem = models.IntegerField()
-	phy = models.IntegerField()
-	cs = models.IntegerField()
-	eng = models.IntegerField()
+class Inward(models.Model):
+	App_name = models.CharField(max_length=20)
+	amount = models.DecimalField(max_digits=15, decimal_places=2)
+	amountINR = models.DecimalField(max_digits=15, decimal_places=2)
+	GLS_commission = models.DecimalField(max_digits=15, decimal_places=2)
+	Ex_commission = models.DecimalField(max_digits=15, decimal_places=2)
+	Service_tax = models.DecimalField(max_digits=15, decimal_places=2)
+	Date = models.Date.Field() 
 	def __str__(self):
-		return self.name
+		return self.App_name
+
+class Inward(models.Model):
+	App_name = models.CharField(max_length=20)
+	Currency = models.CharField(max_length=10)
+	amountForeign = models.DecimalField(max_digits=15, decimal_places=2)
+	amountINR = models.DecimalField(max_digits=15, decimal_places=2)
+	GLS_commission = models.DecimalField(max_digits=15, decimal_places=2)
+	Service_tax = models.DecimalField(max_digits=15, decimal_places=2)
+	Date = models.Date.Field() 
+	def __str__(self):
+		return self.App_name
