5e1b62e724c2dd9969922016671486affa70853e
Jeyasoorya <jeys041297sts@gmail.com>
2017-06-20 17:13:14 +0530

    index page complete

--- dashBoardSBI1/dashB/urls.py
+++ dashBoardSBI1/dashB/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^outward/[a-zA-Z]*/([0-9]*)[.a-z]*/$', views.outward_view, name='outward'),
+    url(r'^inward/[a-zA-Z]*/([0-9]*)[.a-z]*/$', views.inward_view, name='inward'),
     url(r'^$', views.index, name='index'),
 ]
--- dashBoardSBI1/dashB/views.py
+++ dashBoardSBI1/dashB/views.py
@@ -1,31 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.db.models import Sum
 # Create your views here.
 
 def index(request):
 	i_apps = Inward.objects.all()
-	o_no_apps = len(list(Outward.objects.all()))
-	o_comm_list = list(OutwardData.objects.all().order_by('-Date').values_list('commissionAmt'))[0:o_no_apps-1]
-	o_amt_list = list(OutwardData.objects.all().order_by('-Date').values_list('BeneficiaryAmountINR'))[0:o_no_apps-1]
-	o_comm = 0
-	for out in o_comm_list:
-		o_comm = o_comm+out[0]
-	o_amt = 0
-	for out in o_amt_list:
-		o_amt = o_amt+out[0]
-	i_no_apps = len(list(i_apps))
-	i_comm_list = list(InwardData.objects.all().order_by('-Date').values_list('GLS_commission'))[0:i_no_apps-1]
-	i_commex_list = list(InwardData.objects.all().order_by('-Date').values_list('Ex_commission'))[0:i_no_apps-1]
-	i_amt_list = list(InwardData.objects.all().order_by('-Date').values_list('amountINR'))[0:i_no_apps-1]
-	i_comm_gls = 0
-	for i in i_comm_list:
-		i_comm_gls = i_comm_gls+i[0]
-	i_comm_ex = 0
-	for i in i_commex_list:
-		i_comm_ex = i_comm_ex+i[0]
-	i_comm = i_comm_ex+i_comm_gls
-	i_amt = 0
-	for i in i_amt_list:
-		i_amt = i_amt+i[0]
-	return render(request,"dashB/index.html",{"inward":i_apps,"o_comm":o_comm,"o_amt":o_amt,"i_comm":i_comm,"i_amt":i_amt})
+	o_apps = Outward.objects.all().order_by('-id')
+	o_comm_months = list(OutwardData.objects.order_by('Date').values('Date').annotate(cAmt=Sum('commissionAmt')))
+	o_amt_months = list(OutwardData.objects.order_by('Date').values('Date').annotate(amt=Sum('BeneficiaryAmountINR')))
+	i_comm_months = list(InwardData.objects.order_by('Date').values('Date').annotate(cAmt=Sum('GLS_commission')))
+	i_commex_months = list(InwardData.objects.order_by('Date').values('Date').annotate(cexAmt=Sum('Ex_commission')))
+	i_amt_months = list(InwardData.objects.order_by('Date').values('Date').annotate(amt=Sum('amountINR')))
+	for i in range(0,len(i_comm_months)-1):
+		i_comm_months[i]['cAmt']=i_comm_months[i]['cAmt']+i_commex_months[i]['cexAmt']
+	o_comm = o_comm_months[len(o_comm_months)-1]['cAmt']
+	i_comm = i_comm_months[len(i_comm_months)-1]['cAmt']
+	o_amt = o_amt_months[len(o_amt_months)-1]['amt']
+	i_amt = i_amt_months[len(i_amt_months)-1]['amt']
+	return render(request,"dashB/index.html",{"outward":o_apps,"inward":i_apps,"o_comm":o_comm,"i_comm":i_comm,"o_comm_months":o_comm_months,"o_amt":o_amt,"i_comm_months":i_comm_months,"i_amt":i_amt})
+
+def outward_view(request,id):
+	i_apps = Inward.objects.all()
+	o_apps = Outward.objects.all().order_by('-id')
+	return render(request,"dashB/outward.html",{"outward":o_apps,"inward":i_apps})
+
+def inward_view(request,id):
+	i_apps = Inward.objects.all()
+	o_apps = Outward.objects.all().order_by('-id')
+	return render(request,"dashB/inward.html",{"outward":o_apps,"inward":i_apps})
