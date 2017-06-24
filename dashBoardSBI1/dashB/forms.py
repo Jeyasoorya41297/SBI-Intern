c7ebf05e1dccc9c5d2c988932654d6c7d2a003c1
Raunaque <rpatra1997@gmail.com>
2017-06-24 15:03:24 +0530

    date range

--- dashBoardSBI1/dashB/forms.py
+++ dashBoardSBI1/dashB/forms.py
@@ -3,13 +3,20 @@ import datetime
 from .models import *
 
 class Ograph(forms.Form):
-    ch_of_graph = forms.ChoiceField(choices=(('BeneficiaryAmount','Beneficiary Amount'),('BeneficiaryAmountINR','Beneficiary Amount in INR'),('commissionAmt','Commission'),('Service_tax','Service Tax')))
-
+	ch_of_graph = forms.ChoiceField(choices=(('BeneficiaryAmount','Beneficiary Amount'),('BeneficiaryAmountINR','Beneficiary Amount in INR'),('commissionAmt','Commission'),('Service_tax','Service Tax')))
+	in_date=forms.DateField()
+	fi_date=forms.DateField()
 class Igraph(forms.Form):
 	ch_of_graph = forms.ChoiceField(choices=(('amount','Amount'),('amountINR','Amount in INR'),('GLS_commission','GLS Commission'),('Ex_commission','Exchange Commission'),('Service_tax','Service Tax')))
-
+	in_date=forms.DateField()
+	fi_date=forms.DateField()
 class compout(forms.Form):
 	ch_of_graph = forms.ChoiceField(choices=(('BeneficiaryAmountINR','Beneficiary Amount in INR'),('commissionAmt','Commission'),('Service_tax','Service Tax')))
+	in_date=forms.DateField()
+	fi_date=forms.DateField()
 
 class compin(forms.Form):
 	ch_of_graph = forms.ChoiceField(choices=(('amountINR','Amount in INR'),('GLS_commission','GLS Commission'),('Ex_commission','Exchange Commission'),('Service_tax','Service Tax')))
+	in_date=forms.DateField()
+	fi_date=forms.DateField()
+	
