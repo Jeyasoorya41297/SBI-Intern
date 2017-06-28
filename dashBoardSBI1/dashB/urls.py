85cbcb4945adca65bde2d700646756eadc8a3e9b
Jeyasoorya <jeys041297sts@gmail.com>
2017-06-28 11:11:16 +0530

    adjusting html elements

--- dashBoardSBI1/dashB/urls.py
+++ dashBoardSBI1/dashB/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^inward/compin[.a-zA-Z]*/$', views.compIn_view, name='compin'),
-	url(r'^outward/compout[.a-zA-Z]*/$', views.compOut_view, name='compout'),
-    url(r'^outward/[a-zA-Z]*/([0-9]*)[.a-z]*/$', views.outward_view, name='outward'),
-    url(r'^inward/[a-zA-Z]*/([0-9]*)[.a-z]*/$', views.inward_view, name='inward'),
-    url(r'^viewdata$', views.view_data, name='viewData'),
+	url(r'^inwardcomp/$', views.compIn_view, name='compin'),
+	url(r'^outwardcomp/$', views.compOut_view, name='compout'),
+    url(r'^outward([a-zA-Z]*)/$', views.outward_view, name='outward'),
+    url(r'^inward([a-zA-Z]*)/$', views.inward_view, name='inward'),
+    url(r'^viewdata/$', views.view_data, name='viewData'),
     url(r'^$', views.index, name='index'),
 ]
--- dashBoardSBI1/dashB/views.py
+++ dashBoardSBI1/dashB/views.py
@@ -60,10 +60,10 @@ def index(request):
 	return render(request,"dashB/index.html",{"list_of_values":list_of_values,"outward":o_apps,"inward":i_apps,"o_comm_months":o_comm_months,"i_comm_months":i_comm_months})
 
 @csrf_exempt
-def outward_view(request,id):
+def outward_view(request,name):
 	i_apps = Inward.objects.all()
 	o_apps = Outward.objects.all()
-	selected_curr = Outward.objects.get(id=id)
+	selected_curr = Outward.objects.get(Currency=name)
 	if request.method == 'POST':
 		form = Ograph(request.POST)
 		if form.is_valid():
@@ -71,46 +71,50 @@ def outward_view(request,id):
 			in_date=request.POST['in_date']
 			fi_date=request.POST['fi_date']
 
-			x_data = list(OutwardData.objects.filter(curr__id=id,Date__gte=in_date,Date__lte=fi_date).order_by("Date").values("Date",request.POST['ch_of_graph']))
+			x_data = list(OutwardData.objects.filter(curr__Currency=name,Date__gte=in_date,Date__lte=fi_date).order_by("Date").values("Date",request.POST['ch_of_graph']))
 			print(x_data)
 		else :
 			form = Ograph()
 			sel_field='BeneficiaryAmount'
-			x_data = list(OutwardData.objects.filter(curr__id=id).order_by("Date").values("Date",'BeneficiaryAmount'))
-
+			x_data = list(OutwardData.objects.filter(curr__Currency=name).order_by("Date").values("Date",'BeneficiaryAmount'))
+			if len(x_data) > 10:
+				x_data = x_data[-10:]
 	else :
 		form = Ograph()
 		sel_field='BeneficiaryAmount'
-		x_data = list(OutwardData.objects.filter(curr__id=id).order_by("Date").values("Date",'BeneficiaryAmount'))
-	if len(x_data)>7 :
-		x_data = x_data[-7:]
+		x_data = list(OutwardData.objects.filter(curr__Currency=name).order_by("Date").values("Date",'BeneficiaryAmount'))
+		if len(x_data)>10 :
+			x_data = x_data[-10:]
+	
 	return render(request,"dashB/outward.html",{"outward":o_apps,"inward":i_apps,"data":x_data,"form":form,"curr":selected_curr,"sel_field":sel_field})
 
 @csrf_exempt
-def inward_view(request,id):
+def inward_view(request,name):
 	i_apps = Inward.objects.all()
 	o_apps = Outward.objects.all()
-	selected_app = Inward.objects.get(id=id)
+	selected_app = Inward.objects.get(App_name=name)
 	if request.method == 'POST':
 		form = Igraph(request.POST)
 		if form.is_valid():
 			sel_field = request.POST['ch_of_graph']
 			in_date=request.POST['in_date']
 			fi_date=request.POST['fi_date']
-			x_data = list(InwardData.objects.filter(App__id=id,Date__gte=in_date,Date__lte=fi_date).order_by("Date").values("Date",request.POST['ch_of_graph']))
+			x_data = list(InwardData.objects.filter(App__App_name=name,Date__gte=in_date,Date__lte=fi_date).order_by("Date").values("Date",request.POST['ch_of_graph']))
 			print(x_data)
 		else :
 			form = Igraph()
 			sel_field='amountINR'
-			x_data = list(InwardData.objects.filter(App__id=id).order_by("Date").values("Date",'amountINR'))
+			x_data = list(InwardData.objects.filter(App__App_name=name).order_by("Date").values("Date",'amountINR'))
+			if len(x_data) > 10:
+				x_data = x_data[-10:]
 
 	else :
 		form = Igraph()
 		sel_field='amountINR'
-		x_data = list(InwardData.objects.filter(App__id=id).order_by("Date").values("Date",'amountINR'))
-	
-	if len(x_data)>7 :
-		x_data = x_data[-7:]
+		x_data = list(InwardData.objects.filter(App__App_name=name).order_by("Date").values("Date",'amountINR'))
+		if len(x_data) > 10:
+			x_data = x_data[-10:]
+
 	return render(request,"dashB/inward.html",{"outward":o_apps,"inward":i_apps,"data":x_data,"form":form,"app":selected_app,"sel_field":sel_field})
 
 @csrf_exempt
@@ -128,10 +132,14 @@ def compOut_view(request) :
 			form = compout()
 			sel_field = 'BeneficiaryAmountINR'
 			x_data = list(OutwardData.objects.values('curr__Currency').annotate(x_val=Sum(sel_field)))
+			if len(x_data) > 10:
+				x_data = x_data[-10:]
 	else:
 		form = compout()
 		sel_field = 'BeneficiaryAmountINR'
 		x_data = list(OutwardData.objects.values('curr__Currency').annotate(x_val=Sum(sel_field)))
+		if len(x_data) > 10:
+			x_data = x_data[-10:]
 	return render(request,"dashB/compout.html",{"outward":o_apps,"inward":i_apps,"sel_field":sel_field,"x_data":x_data})
 
 @csrf_exempt
@@ -149,10 +157,15 @@ def compIn_view(request) :
 			form = compin()
 			sel_field = 'amountINR'
 			x_data = list(InwardData.objects.values('App__App_name').annotate(x_val=Sum(sel_field)))
+			if len(x_data) > 10:
+				x_data = x_data[-10:]
+
 	else:
 		form = compin()
 		sel_field = 'amountINR'
 		x_data = list(InwardData.objects.values('App__App_name').annotate(x_val=Sum(sel_field)))
+		if len(x_data) > 10 :
+			x_data = x_data[-10:]
 
 	return render(request,"dashB/compin.html",{"outward":o_apps,"inward":i_apps,"sel_field":sel_field,"x_data":x_data})
 
